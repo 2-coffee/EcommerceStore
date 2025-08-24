1e9429122a4ed1b65af756e60e77bc11dae494a3
“kevin” <kevinle1878@gmail.com>
2025-08-23 23:46:00 -0700

    initial build/docker setup

---
+++ user-service/src/test/java/com/__coffee/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.__coffee.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
